80f6cd25000a3c8e008bdc93694a44b72483f531
Zaedul Islam <islam.zaedul@gmail.com>
2023-01-21 22:52:03 -0500

    Create main.cpp

---
+++ Tutorial 1/Exercise 1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "BankAccount.h"
+
+
+int main() {
+    int accountNumber;
+    std::string accountName, accountType;
+    double balance;
+
+    std::cout << "\nEnter Details:\n";
+    std::cout << "-----------------------\n";
+    std::cout << "Account No: ";
+    std::cin >> accountNumber;
+    std::cout << "Name: ";
+    std::cin >> accountName;
+    std::cout << "Account Type: ";
+    std::cin >> accountType;
+    std::cout << "Balance: ";
+    std::cin >> balance;
+
+    BankAccount b1(accountNumber, accountName, accountType, balance);  // "b1" object is created
+    b1.depositAmount(); //
+    b1.withdrawAmount(); // Calling member functions
+    b1.displayAccountDetails(); //
+
+    return 0;
+}
