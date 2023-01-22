9a266b6cea84d88b5d7e9b5cef5335c216dbb085
Zaedul Islam <islam.zaedul@gmail.com>
2023-01-21 22:53:18 -0500

    Create BankAccount.h

---
+++ Tutorial 1/Exercise 1/BankAccount.h
@@ -0,0 +1,34 @@
+//
+// Created by Zaedul Islam on 2023-01-14.
+//
+
+#ifndef CPP_BANKACCOUNT_H
+#define CPP_BANKACCOUNT_H
+
+
+class BankAccount
+{
+    private:
+        // Data Members
+        int accountNumber;
+        std::string accountName;
+        std::string accountType;
+        double balance;
+
+    public:
+        // Default Constructor
+        BankAccount();
+
+        // Parameterized Constructor
+        BankAccount(int accountNumber, std::string accountName, std::string accountType, double balance);
+
+        // Member Functions
+        // Deposit an amount
+        void depositAmount();
+        // Withdraw an amount
+        void withdrawAmount();
+        // Display account details
+        void displayAccountDetails();
+};
+
+#endif //CPP_BANKACCOUNT_H
